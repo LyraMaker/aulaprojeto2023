d5cb250bf1cccd2a2fb11e3f504dd29b4d8007a6
LyraMaker <gabriel.lyrat@gmail.com>
2023-08-29 11:36:59 -0300

    Desafio PDO - Criado um arquivo só para cadastrar

--- desafio pdo/instancia.php
+++ desafio pdo/instancia.php
@@ -3,12 +3,7 @@
 require "conexao.php";
 require "Autor.php";
 require "RepositorioAutor.php";
-
-//$autor = new Autor("002","Dora Raposo","Rua XYZ","(21) 9090-9090","email@email.com");
-
 $repAutor = new RepositorioAutor();
-
-//$repAutor->cadastrar($banco,$autor);
 
 $valores = $repAutor->exibirTudo($banco);
 
